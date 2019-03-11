d71f298f858114005e950f434bfed0fb2d48d2e3
borjasolerr <borjasolrey@gmail.com>
2019-03-11 23:07:12 +0100

    Added individualReducers.js

--- irs/src/actions/actionTypes.js
+++ irs/src/actions/actionTypes.js
@@ -2,3 +2,4 @@ export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const LOADING_ON = 'LOADING_ON';
 export const LOADING_OFF = 'LOADING_OFF';
+export const ADD_INFO = 'ADD_INFO';
