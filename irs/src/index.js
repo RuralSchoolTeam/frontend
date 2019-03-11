1bd6410de8b8877250749648537a9e4b64f86a04
borjasolerr <borjasolrey@gmail.com>
2019-03-11 23:13:48 +0100

    Imported actions to index.js and added middleware to save user token

--- irs/src/index.js
+++ irs/src/index.js
@@ -8,8 +8,17 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import * as types from './actions/actionTypes';
+
 // Style
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const middlewareToSaveUserToken = store => next => action => {
+  if (action.type === types.LOGIN) {
+    localStorage.setItem('token', action.payload);
+  }
+  next(action);
+};
 
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
