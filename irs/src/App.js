8991f694d70e391aeb352e2d0aecfb0eeecb3514
borjasolerr <borjasolrey@gmail.com>
2019-03-11 21:25:27 +0100

    Added register route

--- irs/src/App.js
+++ irs/src/App.js
@@ -3,12 +3,14 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import Login from './components/Login/Login';
+import Register from './components/Login/Register';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
       </div>
     );
   }
