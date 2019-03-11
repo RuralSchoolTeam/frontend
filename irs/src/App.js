12d789646343595a2bd33625c37f96ebbe0358e4
borjasolerr <borjasolrey@gmail.com>
2019-03-11 21:39:33 +0100

    Added component whoyouare, page after doing login

--- irs/src/App.js
+++ irs/src/App.js
@@ -3,7 +3,8 @@ import { Route } from 'react-router-dom';
 import './App.css';
 
 import Login from './components/Login/Login';
-import Register from './components/Login/Register';
+import Register from './components/Registration/Register';
+import Whoyouare from './components/Whoyouare/Whoyouare';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/whoyouare" component={Whoyouare} />
       </div>
     );
   }
