a205396f19308952fa0abe319a8174a0f7fd1e86
ddot727 <ddot727@gmail.com>
2019-03-12 15:38:42 -0400

    created actions for registering, moving on to login bugs

--- irs/src/actions/actionTypes.js
+++ irs/src/actions/actionTypes.js
@@ -3,3 +3,7 @@ export const LOGOUT = 'LOGOUT';
 export const LOADING_ON = 'LOADING_ON';
 export const LOADING_OFF = 'LOADING_OFF';
 export const ADD_INFO = 'ADD_INFO';
+
+export const REGISTERING = "REGISTERING";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
