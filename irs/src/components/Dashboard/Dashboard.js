246d4691fbb27b430ec6aa733b8edbf297c6ecc0
borjasolerr <borjasolrey@gmail.com>
2019-03-13 15:51:33 +0100

    Added dashboard component after merging conflicts

---
+++ irs/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import NavBarSide from './leftSidebar';
+
+const DashboardPage = () => (
+  <div>
+    <h1>Hello</h1>
+    <NavBarSide />
+  </div>
+);
+
+export default DashboardPage;
