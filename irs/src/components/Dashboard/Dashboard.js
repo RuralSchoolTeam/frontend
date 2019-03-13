9e9c5afc1779b1cdf3a25abc4ae6a3370f71a455
borjasolerr <borjasolrey@gmail.com>
2019-03-13 18:43:28 +0100

    Added form for issues and posibility to see issues in the dashboard

--- irs/src/components/Dashboard/Dashboard.js
+++ irs/src/components/Dashboard/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBarSide from './leftSidebar';
+import IssuesCards from './IssuesCards';
 
 const DashboardPage = () => (
   <div>
     <h1>Hello</h1>
     <NavBarSide />
+    <IssuesCards />
   </div>
 );
 
